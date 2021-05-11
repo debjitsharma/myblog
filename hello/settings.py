e10f5dce367d75c74a33bf90386425a5a18b6f00
Debjit <debjit2k15@gmail.com>
2021-05-11 19:05:08 +0530

    final6 commit

--- hello/settings.py
+++ hello/settings.py
@@ -141,3 +141,7 @@ MEDIA_URL= '/media/'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
