11402c3b777c150f2248bc2f9f76c4f04839f040
Debjit <debjit2k15@gmail.com>
2021-05-11 16:59:34 +0530

    final1 commit

--- hello/settings.py
+++ hello/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n3$uy_*8+m@d47v6534cbbkoa^0+wy3!3&)63t0v3785bje(6q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['blgprj.herokuapp.com','127.0.0.1:8000']
 
